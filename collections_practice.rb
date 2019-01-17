8afa35651f8260edc18ae6076c3459fcb2936287
Mina Mekhael <mina.mekhael@ymail.com>
2019-01-17 23:13:15 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,30 @@
+def sort_array_asc(array)
+  array.sort!
+end
 
+def sort_array_desc(array)
+  array.sort_by do |sort|
+    -sort
+end
+end
+
+def sort_array_char_count(array)
+  array.sort_by do |sort|
+    sort.length
+  end
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each do |item|
+    item[2]= "$"
+  end
+end
